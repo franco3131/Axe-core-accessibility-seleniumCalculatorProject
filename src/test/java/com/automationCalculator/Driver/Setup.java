136957e428f7d1f4d8c211d5d968f6e509e60349
David Franco <franco3131@yahoo.com>
2021-04-05 00:39:01 -0700

    Add files via upload

---
+++ src/test/java/com/automationCalculator/Driver/Setup.java
@@ -0,0 +1,25 @@
+package com.automationCalculator.Driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Setup {
+	public static WebDriver driver;
+	@Before
+	public void setup() {
+	  System.setProperty("webdriver.chrome.driver", "/Users/davidfranco/eclipse-workspace/mavenProject2/calculatorProject/WebDrivers/chromedriver 3");
+      driver = new ChromeDriver();
+	  DesiredCapabilities cap = DesiredCapabilities.chrome();
+      cap.setCapability("applicationCacheEnabled", false);
+	  driver.manage().window().maximize();
+		  System.out.println("hi");
+	}
+	@After
+	public void tearDown() {
+		  driver.quit();
+	}
+}
