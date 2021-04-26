1152daab72424dadd2c081fa8ddbb715078c589d
David Franco <franco3131@yahoo.com>
2021-04-25 22:59:17 -0700

    Add files via upload

--- src/test/java/com/automationCalculator/Driver/Setup.java
+++ src/test/java/com/automationCalculator/Driver/Setup.java
@@ -15,7 +15,7 @@ public class Setup {
 	  System.setProperty("webdriver.chrome.driver", "/Users/davidfranco/eclipse-workspace/mavenProject2/calculatorProject/WebDrivers/chromedriver 3");
 	 }
 	 else {
-		  System.setProperty("webdriver.chrome.driver", "/Users/davidfranco/eclipse-workspace/mavenProject2/calculatorProject/WebDrivers/chromedriver");
+		  System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 
 	 }
 	  driver = new ChromeDriver();
