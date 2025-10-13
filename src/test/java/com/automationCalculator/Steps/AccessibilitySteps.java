4493538137bb2fcfc39309368ddf01aaa5fdd2b1
David Franco <franco3131@yahoo.com>
2025-10-12 23:49:58 -0700

    Create AccessibilitySteps.java

---
+++ src/test/java/com/automationCalculator/Steps/AccessibilitySteps.java
@@ -0,0 +1,15 @@
+package com.automationCalculator.Steps;
+
+import com.automationcalculator.accessibility.AxeChecks;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.WebDriver;
+
+import static com.automationcalculator.support.Hooks.driver; // wherever you keep WebDriver
+
+public class AccessibilitySteps {
+    @Then("the page has no WCAG AA accessibility violations")
+    public void thePageHasNoWcagAAViolations() {
+        WebDriver d = driver; // get your active driver
+        AxeChecks.assertNoWcagAA(d, d.getTitle().replaceAll("\\W+","_"));
+    }
+}
