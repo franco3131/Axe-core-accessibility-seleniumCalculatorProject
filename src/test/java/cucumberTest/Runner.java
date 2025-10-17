d8b8b7f9ba799fd91e41eb2917555c53532ba960
David Franco <franco3131@yahoo.com>
2025-10-16 23:43:12 -0700

    Update Runner.java

--- src/test/java/cucumberTest/Runner.java
+++ src/test/java/cucumberTest/Runner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
   features = "src/test/resources/Features",
-  glue = {"com.automationCalculator"},
+  glue = {"com.automationCalculator.Steps","com.automationCalculator.Driver"},
   plugin = {"pretty","json:target/cucumber.json"},
   monochrome = true
 )
